d2edbdc23e47fb67b17ae068021fd2bf444300e2
Joao Gabriel <j.gomes1154@gmail.com>
2019-07-10 11:36:51 -0300

    ic

---
+++ app/src/main/java/com/example/danilo/appdebts/Database/insertDebts.java
@@ -0,0 +1,13 @@
+package com.example.danilo.appdebts.Database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class insertDebts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_debts);
+    }
+}
